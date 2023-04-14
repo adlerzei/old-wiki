ba1a71c6a6ae75d3f200c9bad2d2381dbb5dc133
torma616 <torma616@gmail.com>
2023-04-14 17:09:10 -0400

    Updated logic in case other queries in use

--- extension/updateAddress.js
+++ extension/updateAddress.js
@@ -8,10 +8,17 @@
 // to bring us back to the good old days when
 // Vector was default, as it should be.
 //
-if (document.location.search === '?useskin=vector') {
+
+let queryString = document.location.search;
+
+if (queryString.includes('useskin=vector')) {
+    queryString = queryString.replace(/(|&)useskin=vector(&|)/, '');
+    if (queryString === '?') {
+        queryString = '';
+    }
   window.history.replaceState(
     null, 
     document.title, 
-    `${document.location.origin}${document.location.pathname}${document.location.hash}`
+    `${document.location.origin}${document.location.pathname}${queryString}${document.location.hash}`
     )
 }
